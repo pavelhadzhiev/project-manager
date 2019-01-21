8462446ba721d57d77e4654a1c7588fe74fc128b
Pavel Hadzhiev <p.hadzhiev96@gmail.com>
2019-01-21 22:18:44 +0200

    Fix a bug with undefined session variable in the php script.

--- php/populateTaskColumns.php
+++ php/populateTaskColumns.php
@@ -5,34 +5,41 @@
     
     $db = new PDO('mysql:host=localhost;dbname=FMI;charset=utf8', 'root', '');
 
-    $fetchAll = "SELECT * FROM tasks WHERE projectName='".$_SESSION['project']."'";
-    $fetchAllQuery = $db->query($fetchAll) or die("failed!");
-    $tasks = $fetchAllQuery->fetchAll();
+    $toDoTasksHtml = '';
+    $inProgressTasksHtml = '';
+    $forReviewTasksHtml = '';
+    $doneTasksHtml = '';
 
-    $toDoTasksHtml = '<ul id="toDoTasks" class="taskColumn bordered">';
-    $inProgressTasksHtml = '<ul id="inProgressTasks" class="taskColumn bordered">';
-    $forReviewTasksHtml = '<ul id="forReviewTasks" class="taskColumn bordered">';
-    $doneTasksHtml = '<ul id="doneTasks" class="taskColumn bordered">';
-    foreach ($tasks as $value) {
-        $htmlString = "<li class='task bordered'> Task ".$value['id']." - ".$value['title']."</li>";
-        switch ($value['status']) {
-            case 'toDo':
-                $toDoTasksHtml .= $htmlString;
+    if(isset($_SESSION['project'])) {
+        $fetchAll = "SELECT * FROM tasks WHERE projectName='".$_SESSION['project']."'";
+        $fetchAllQuery = $db->query($fetchAll) or die("failed!");
+        $tasks = $fetchAllQuery->fetchAll();
+
+        $toDoTasksHtml .= '<ul id="toDoTasks" class="taskColumn bordered">';
+        $inProgressTasksHtml .= '<ul id="inProgressTasks" class="taskColumn bordered">';
+        $forReviewTasksHtml .= '<ul id="forReviewTasks" class="taskColumn bordered">';
+        $doneTasksHtml .= '<ul id="doneTasks" class="taskColumn bordered">';
+        foreach ($tasks as $value) {
+            $htmlString = "<li class='task bordered'> Task ".$value['id']." - ".$value['title']."</li>";
+            switch ($value['status']) {
+                case 'toDo':
+                    $toDoTasksHtml .= $htmlString;
+                    break;
+                case 'inProgress':
+                    $inProgressTasksHtml .= $htmlString;
                 break;
-            case 'inProgress':
-                $inProgressTasksHtml .= $htmlString;
-            break;
-            case 'forReview':
-                $forReviewTasksHtml .= $htmlString;
-                break;
-            case 'done':
-                $doneTasksHtml .= $htmlString;
-                break;
+                case 'forReview':
+                    $forReviewTasksHtml .= $htmlString;
+                    break;
+                case 'done':
+                    $doneTasksHtml .= $htmlString;
+                    break;
+            }
         }
-    }
 
-    $toDoTasksHtml .= '</ul>';
-    $inProgressTasksHtml .= '</ul>';
-    $forReviewTasksHtml .= '</ul>';
-    $doneTasksHtml .= '</ul>';
+        $toDoTasksHtml .= '</ul>';
+        $inProgressTasksHtml .= '</ul>';
+        $forReviewTasksHtml .= '</ul>';
+        $doneTasksHtml .= '</ul>';
+    }
 ?>
